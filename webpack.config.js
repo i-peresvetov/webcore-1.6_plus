6d2dbe5e02a9bc6d6ed3ccb08950687fa3454bb7
Ivan Peresvetov <lumenist@mail.ru>
2023-10-18 23:26:32 +0300

    6860

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 const path = require("path");
-const HTMLWebpackPlugin = require('html-webpack-plugin') //подключил возможность запустить плагин
-const {CleanWebpackPlugin} = require('clean-webpack-plugin') //подключил возможность запустить плагин
+const HTMLWebpackPlugin = require('html-webpack-plugin') //подключение возможности запустить плагин
+const {CleanWebpackPlugin} = require('clean-webpack-plugin') //подключение возможности запустить плагин
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const isDev = process.env.NODE_ENV === 'development' // проверка режима работы
+const isProd = !isDev // проверка режима работы
+// console.log('IS DEV: ', isDev)
 
 module.exports = {
   context: path.resolve(__dirname, 'src'), //указываю стартовую папку где искать файлы
   mode: "development", //режим результата упаковки, сейчас дев вариант с комментами
+  // второй mode это "production" в котором всё коротко и упаковано
   entry: {    // точка входа, могут быть несколько
     main: "./index.js", //это точка входа с названием main
     analytics: "./analytics.js", //это точка входа с названием analytics
@@ -23,7 +29,8 @@ module.exports = {
     }
   },
   devServer: { //использует пакет webpack-dev-server
-    port: 4200
+    port: 4200,
+    hot: isDev // горячая замена модулей
   },
   optimization: {
     splitChunks: { //без этого подключаемые библиотеки будут паковаться каждый раз при использовании
@@ -33,7 +40,10 @@ module.exports = {
   plugins: [ //список подключенных плагинов
     new HTMLWebpackPlugin({ //автоматически добавляет JS в HTML и создаёт index.html при сборке
         //title: 'Webpack Lum', //имя странички на выходе, не работает если есть template
-        template: './index.html' //на основании какого файла HTML создавать бандл
+        template: './index.html', //на основании какого файла HTML создавать бандл
+        minify: { // настройки минификации HTML (вроде в вебпак 5 работает по умолчанию)
+          collapseWhitespace: isProd //убрать пробелы
+        }
     }),
     new CleanWebpackPlugin(), //очищает dist
     new CopyWebpackPlugin({ //плагин для копирования файлов
@@ -43,6 +53,9 @@ module.exports = {
           to: path.resolve(__dirname, 'dist') //куда копирую
         }
       ]
+    }),
+    new MiniCssExtractPlugin({  // без активации тут не будет работать в лоадерах
+      filename: "[name].[contenthash].css"
     })
   ],
 
@@ -53,10 +66,16 @@ module.exports = {
             //если вебпак встречает ТАКОЙ файл
             test: /\.css$/,
             //то надо использовать вот эти лоадеры (пропускает через себя справа налево)
-            use: ['style-loader', 'css-loader']
+            //use: ['style-loader', 'css-loader']
             //лодеры это тоже пакеты     
             //css-loader позволяет понимать импорты CSS в JS
             //style-loader добавляет стили в хедер
+            use: [{
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+
+              }
+            }, 'css-loader'] // замена style-loader на minicssextract для того что бы css был в отдельном файле
         },
         {
             test: /\.(png|jpg|svg|gif)$/,            
